a1173858682ee9dc9fd11091a056df3f390bba2f
Damian Melniczuk <damian.melniczuk@gmail.com>
2013-05-26 10:09:31 +0200

    fixed bud with to high humidity value at firs readout

--- server.py
+++ server.py
@@ -36,19 +36,22 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 		split_data = self.path[1:12].split(";")
 		hum = split_data[0]
 		cels = split_data[1]
-		#temperatura na zewnatrz:
-		f = urllib2.urlopen('http://api.wunderground.com/api/a105856cea4d589b/conditions/q/pws:IDOLNOU013.json') 
-		json_string = f.read() 
-		parsed_json = json.loads(json_string) 
-		temp_c = parsed_json['current_observation']['temp_c'] 
-		#print "Current temperature is: %s" % (temp_c) 
-		f.close()
-		#koniec temperatury na zewnatrz
-		with con:
-		    cur = con.cursor()
-		    query = """INSERT INTO temperatures (date, temperature, temperature_outside , location, humidity) VALUES (datetime('now', 'localtime'), ?, ?, ?, ?)"""
-		    data =  [cels,temp_c,'sypialnia',hum]
-		    cur.execute(query, data)
+		if float(hum)>100:
+			pass
+		else:
+			#temperatura na zewnatrz:
+			f = urllib2.urlopen('http://api.wunderground.com/api/a105856cea4d589b/conditions/q/pws:IDOLNOU013.json') 
+			json_string = f.read() 
+			parsed_json = json.loads(json_string) 
+			temp_c = parsed_json['current_observation']['temp_c'] 
+			#print "Current temperature is: %s" % (temp_c) 
+			f.close()
+			#koniec temperatury na zewnatrz
+			with con:
+			    cur = con.cursor()
+			    query = """INSERT INTO temperatures (date, temperature, temperature_outside , location, humidity) VALUES (datetime('now', 'localtime'), ?, ?, ?, ?)"""
+			    data =  [cels,temp_c,'sypialnia',hum]
+			    cur.execute(query, data)
 
 
 BaseHTTPServer.HTTPServer(('', 12345), Handler).serve_forever() 
